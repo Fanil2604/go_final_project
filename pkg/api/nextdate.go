4015231e7813ed3bc41183343ef6b16dcf5eb796
Fanil <fanil0082@gmail.com>
2025-06-03 23:02:01 +0500

    fix

--- pkg/api/nextdate.go
+++ pkg/api/nextdate.go
@@ -1,39 +1,70 @@
 package api
 
 import (
-	"fmt"
+	"errors"
 	"strconv"
-	"strings"
 	"time"
 )
 
-func NextDate(now time.Time, dstart string, repeat string) (string, error) {
-	// Парсинг текущей даты
-	currentDate, err := time.Parse(DateFormat, dstart)
+func NextDate(nowtime time.Time, dstart string, repeat string) (string, error) {
+	//проверка пустой или нет repeat
+	if len(repeat) == 0 {
+
+		return "", errors.New("repeat is empty")
+	}
+
+	//разбираем dstart на соответствие формата
+	parsedTime, err := time.Parse(DateFormat, dstart)
 	if err != nil {
-		return "", err
+		return "", errors.New("error parse dstart")
 	}
 
-	// Логика для разных правил повторения
-	switch repeat {
-	case "y":
-		// Если правило y, то добавляем год
-		nextDate := currentDate.AddDate(1, 0, 0)
-
-		return nextDate.Format(DateFormat), nil
-	default:
-		if strings.HasPrefix(repeat, "d") {
-			parts := strings.Split(repeat, " ")
-			if len(parts) == 2 {
-				days, err := strconv.Atoi(parts[1])
-				if err == nil {
-					nextDate := currentDate.Add(time.Duration(days) * 24 * time.Hour)
-					return nextDate.Format(DateFormat), nil
-				}
-			}
+	//если другой формат repeat. в моём случае только 2 варианта "d " и "y"
+	if len(repeat) > 3 {
+		if repeat[:2] != "d " {
+			//fmt.Println("error format repeat")
+			return "", errors.New("error format repeat")
 		}
-		// Если не указано правило или оно неверно
-		return "", fmt.Errorf("неверное правило повторения: %s", repeat)
+
 	}
+
+	if len(repeat) == 1 {
+		if repeat != "y" {
+			//fmt.Println("error format repeat")
+			return "", errors.New("error format repeat")
+		}
+	}
+
+	//если repeat == y , т.е. прибавить год
+	if repeat == "y" {
+		for {
+			parsedTime = parsedTime.AddDate(1, 0, 0)
+			if parsedTime.After(nowtime) {
+				break
+			}
+
+		}
+		return parsedTime.Format(DateFormat), nil
+	}
+
+	repeat = repeat[2:] // Удаляем первые 2 символа
+	//если repeat цифра
+	num, err := strconv.Atoi(repeat)
+	if err != nil {
+		return "", errors.New("wrong format repeat")
+	}
+
+	if num > 400 {
+		return "", errors.New("repeat > 400")
+	}
+
+	for {
+		parsedTime = parsedTime.AddDate(0, 0, num)
+		if parsedTime.After(nowtime) {
+			break
+		}
+	}
+
+	return parsedTime.Format(DateFormat), nil
 
 }
